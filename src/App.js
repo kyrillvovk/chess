0d709a2c0f5d7f7f8188e4bc397163c486c5c4d8
Kyrill Vovk <kyrill07.vovk@gmail.com>
2023-05-30 20:58:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import logo from './logo.svg';
+import './App.css';
+import Figure from './Figure';
+import { useEffect, useState } from 'react';
+
+import boardImage from './Chessboard480.png';
+import whiteKing from './figures/Chess_klt45.svg'
+import whiteQueen from './figures/Chess_qlt45.svg'
+import whiteRook from './figures/Chess_rlt45.svg'
+import whiteBishop from './figures/Chess_blt45.svg'
+import whiteKnight from './figures/Chess_nlt45.svg'
+import whitePawn from './figures/Chess_plt45.svg'
+
+import blackKing from './figures/Chess_kdt45.svg'
+import blackQueen from './figures/Chess_qdt45.svg'
+import blackRook from './figures/Chess_rdt45.svg'
+import blackBishop from './figures/Chess_bdt45.svg'
+import blackKnight from './figures/Chess_ndt45.svg'
+import blackPawn from './figures/Chess_pdt45.svg'
+
+function App() {
+    const [board, setBoard] = useState([]);
+    const [counterWhite, setCounterWhite] = useState(0);
+    const [counterBlack, setCounterBlack] = useState(0);
+    const [stepsWhite, setStepsWhite] = useState([]); 
+    const [stepsBlack, setStepsBlack] = useState([]);
+
+    useEffect(() => {
+        setBoard([
+            ['wr1', 'wn1', 'wb1', 'wk', 'wq', 'wb2', 'wn2', 'wr2'],
+            ['wp1', 'wp2', 'wp3', 'wp4', 'wp5', 'wp6', 'wp7', 'wp8'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['bp1', 'bp2', 'bp3', 'bp4', 'bp5', 'bp6', 'bp7', 'bp8'],
+            ['br1', 'bn1', 'bb1', 'bk', 'bq', 'bb2', 'bn2', 'br2'],
+        ]);
+    }, []);
+
+    return (
+        <>
+            <div style={{ backgroundImage: `url(${boardImage})`, width: 480, height: 480 }}>
+                <Figure startPosition={[0, 3]} id="wk" key={'whiteKing'} type={'king'} figure={whiteKing} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 4]} id="wq" key={'whiteQueen'} type={'queen'} figure={whiteQueen} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 0]} id="wr1" key={'whiteRook1'} type={'rook'} figure={whiteRook} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 7]} id="wr2" key={'whiteRook2'} type={'rook'} figure={whiteRook} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 2]} id="wb1" key={'whiteBishop1'} type={'bishop'} figure={whiteBishop} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 5]} id="wb2" key={'whiteBishop2'} type={'bishop'} figure={whiteBishop} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 1]} id="wn1" key={'whiteKnight1'} type={'knight'} figure={whiteKnight} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[0, 6]} id="wn2" key={'whiteKnight2'} type={'knight'} figure={whiteKnight} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 0]} id="wp1" key={'whitePawn1'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 1]} id="wp2" key={'whitePawn2'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 2]} id="wp3" key={'whitePawn3'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 3]} id="wp4" key={'whitePawn4'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 4]} id="wp5" key={'whitePawn5'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 5]} id="wp6" key={'whitePawn6'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 6]} id="wp7" key={'whitePawn7'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[1, 7]} id="wp8" key={'whitePawn8'} type={'whitePawn'} figure={whitePawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                
+                <Figure startPosition={[7, 3]} id="bk" key={'blackKing'} type={'king'} figure={blackKing} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 4]} id="bq" key={'blackQueen'} type={'queen'} figure={blackQueen} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 0]} id="br1" key={'blackRook1'} type={'rook'} figure={blackRook} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 7]} id="br2" key={'blackRook2'} type={'rook'} figure={blackRook} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 2]} id="bb1" key={'blackBishop1'} type={'bishop'} figure={blackBishop} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 5]} id="bb2" key={'blackBishop2'} type={'bishop'} figure={blackBishop} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 1]} id="bn1" key={'blackKnight1'} type={'knight'} figure={blackKnight} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[7, 6]} id="bn2" key={'blackKnight2'} type={'knight'} figure={blackKnight} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 0]} id="bp1" key={'blackPawn1'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 1]} id="bp2" key={'blackPawn2'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 2]} id="bp3" key={'blackPawn3'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 3]} id="bp4" key={'blackPawn4'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 4]} id="bp5" key={'blackPawn5'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 5]} id="bp6" key={'blackPawn6'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 6]} id="bp7" key={'blackPawn7'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+                <Figure startPosition={[6, 7]} id="bp8" key={'blackPawn8'} type={'blackPawn'} figure={blackPawn} board={board} setBoard={setBoard} counterWhite={counterWhite} setCounterWhite={setCounterWhite} counterBlack={counterBlack} setCounterBlack={setCounterBlack} stepsWhite={stepsWhite} setStepsWhite={setStepsWhite} stepsBlack={stepsBlack} setStepsBlack={setStepsBlack}/>
+            </div>
+        <p>
+            {stepsWhite.map((step, index) => <span key={index}>{step}; </span>)}
+        </p>
+        <p>
+            {stepsBlack.map((step, index) => <span key={index}>{step}; </span>)}
+        </p>
+        </>
+    );
+
+    
+}
+
+export default App;
---
+++ src/Figure.js
@@ -0,0 +1,265 @@
+import React, { useEffect, useState } from 'react';
+
+function Figure({ figure, startPosition, type, board, setBoard, id, counterWhite, setCounterWhite, counterBlack, setCounterBlack,  stepsWhite, setStepsWhite, stepsBlack, setStepsBlack}){
+    const [x, setX] = useState(startPosition[0]);
+    const [y, setY] = useState(startPosition[1]);
+    const [isHidden, setIsHidden] = useState(false);
+
+    const refreshBoard = (xPrev, yPrev, xNew, yNew) => {
+        console.log([xPrev, yPrev, xNew, yNew]);
+
+        const boardNew = [];
+
+        if (counterWhite === counterBlack && id[0] === 'w'){
+            setCounterWhite(counterWhite + 1);
+            setStepsWhite([...stepsWhite, id[1] + String.fromCharCode('h'.charCodeAt(0) - yNew) + (xNew+1)]);
+        }
+
+        if (counterWhite === counterBlack+1 && id[0] === 'b'){
+            setCounterBlack(counterBlack + 1);
+            setStepsBlack([...stepsBlack, id[1] + String.fromCharCode('h'.charCodeAt(0) - yNew) + (xNew+1)]);
+        }
+
+        console.log(board[xNew][yNew])
+
+        for (let i = 0; i < board.length; i++) {
+            boardNew.push([]);
+            for (let j = 0; j < board[i].length; j++) {
+                if (i === xPrev && j === yPrev) {
+                    boardNew[i].push('');
+                } else if (i === xNew && j === yNew) {
+                    boardNew[i].push(board[xPrev][yPrev]);
+                } else {
+                    boardNew[i].push(board[i][j]);
+                }
+            }
+            console.log(boardNew[i]);
+        }
+
+        setBoard(boardNew);
+    };
+
+    useEffect(() => {
+        let newState = true;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (board[i][j] === id) {
+                    newState = false;
+                    break;
+                }
+            }
+            if (!newState) {
+                break;
+            }
+        }
+        setIsHidden(newState);
+    }, [board])
+
+    const handleDragEnd = (event) => {
+        const [xPrev, yPrev] = [x, y];
+        const [xNew, yNew] = [
+            Math.floor(event.clientY / 60),
+            Math.floor(event.clientX / 60)
+        ]
+
+        if (xNew < 0 || xNew > 7 || yNew < 0 || yNew > 7) {
+            return;
+        }
+
+        if (board[xNew][yNew] !== '' && id[0] === board[xNew][yNew][0]) {
+            return;
+        }
+
+        if (counterWhite === counterBlack && id[0] === 'b'){
+            return;
+        }
+
+        if (counterWhite === counterBlack+1 && id[0] === 'w'){
+            return;
+        }
+
+
+
+        switch (type) {
+            case 'king':
+                if (Math.abs(xNew - x) <= 1 && Math.abs(yNew - y) <= 1) {
+                    setX(xNew);
+                    setY(yNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                }
+                break;
+
+            case 'queen':
+                if ((yNew - y === 0) || (xNew - x === 0)) {
+                    const path = [];
+                    if (xNew - x !== 0) {
+                        const [start, end] = [
+                            Math.min(xNew, x),
+                            Math.max(xNew, x)
+                        ];
+                        for (let i = start + 1; i < end; i++) {
+                            path.push(board[i][y]);
+                        }
+                    } else {
+                        const [start, end] = [
+                            Math.min(yNew, y),
+                            Math.max(yNew, y)
+                        ];
+                        for (let i = start + 1; i < end; i++) {
+                            path.push(board[x][i]);
+                        }
+                    }
+                    if (path.every(cell => cell === '')) {
+                        setX(xNew);
+                        setY(yNew);
+                        refreshBoard(xPrev, yPrev, xNew, yNew);
+                    }
+                } else if (Math.abs(xNew - x) === Math.abs(yNew - y)) {
+                    const path = [];
+                    if (xNew - x > 0 && yNew - y > 0) {
+                        for (let i = 1; i < xNew - x; i++) {
+                            path.push(board[x + i][y + i]);
+                        }
+                    } else if (xNew - x > 0 && yNew - y < 0) {
+                        for (let i = 1; i < xNew - x; i++) {
+                            path.push(board[x + i][y - i]);
+                        }
+                    } else if (xNew - x < 0 && yNew - y > 0) {
+                        for (let i = 1; i < x - xNew; i++) {
+                            path.push(board[x - i][y + i]);
+                        }
+                    } else {
+                        for (let i = 1; i < x - xNew; i++) {
+                            path.push(board[x - i][y - i]);
+                        }
+                    }
+                    if (path.every(cell => cell === '')) {
+                        setX(xNew);
+                        setY(yNew);
+                        refreshBoard(xPrev, yPrev, xNew, yNew);
+                    }
+                }
+                break;
+
+            case 'rook':
+                if ((yNew - y === 0) || (xNew - x === 0)) {
+                    const path = [];
+                    if (xNew - x !== 0) {
+                        const [start, end] = [
+                            Math.min(xNew, x),
+                            Math.max(xNew, x)
+                        ];
+                        for (let i = start + 1; i < end; i++) {
+                            path.push(board[i][y]);
+                        }
+                    } else {
+                        const [start, end] = [
+                            Math.min(yNew, y),
+                            Math.max(yNew, y)
+                        ];
+                        for (let i = start + 1; i < end; i++) {
+                            path.push(board[x][i]);
+                        }
+                    }
+                    if (path.every(cell => cell === '')) {
+                        setX(xNew);
+                        setY(yNew);
+                        refreshBoard(xPrev, yPrev, xNew, yNew);
+                    }
+                }
+                break;
+
+            case 'bishop':
+                if (Math.abs(xNew - x) === Math.abs(yNew - y)) {
+                    const path = [];
+                    if (xNew - x > 0 && yNew - y > 0) {
+                        for (let i = 1; i < xNew - x; i++) {
+                            path.push(board[x + i][y + i]);
+                        }
+                    } else if (xNew - x > 0 && yNew - y < 0) {
+                        for (let i = 1; i < xNew - x; i++) {
+                            path.push(board[x + i][y - i]);
+                        }
+                    } else if (xNew - x < 0 && yNew - y > 0) {
+                        for (let i = 1; i < x - xNew; i++) {
+                            path.push(board[x - i][y + i]);
+                        }
+                    } else {
+                        for (let i = 1; i < x - xNew; i++) {
+                            path.push(board[x - i][y - i]);
+                        }
+                    }
+                    if (path.every(cell => cell === '')) {
+                        setX(xNew);
+                        setY(yNew);
+                        refreshBoard(xPrev, yPrev, xNew, yNew);
+                    }
+                }
+                break;
+
+            case 'knight':
+                if ((Math.abs(xNew - x) === 2 && Math.abs(yNew - y) === 1) || (Math.abs(xNew - x) === 1 && Math.abs(yNew - y) === 2)) {
+                    setX(xNew);
+                    setY(yNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                }
+                break;
+
+            case 'whitePawn':
+                if (x === 1 && xNew - x === 2 && board[2][y] === '' && yNew - y === 0) {
+                    setX(xNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                } else if (xNew - x === 1 && yNew - y === 0 && board[xNew][yNew] === '') {
+                    setX(xNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                } else if (board[xNew][yNew][0] === 'b' && xNew-x === 1 && Math.abs(yNew-y)===1){
+                    setX(xNew);
+                    setY(yNew);
+                    setStepsWhite([...stepsWhite, id[1] + 'x' +String.fromCharCode('h'.charCodeAt(0) - yNew) + (xNew+1)]);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                }
+                break;
+
+            case 'blackPawn':
+                if (x === 6 && xNew - x === -2 && board[5][y] === '' && yNew - y === 0) {
+                    setX(xNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                } else if (xNew - x === -1 && yNew - y === 0 && board[xNew][yNew] === '') {
+                    setX(xNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                }else if (board[xNew][yNew][0] === 'w' && xNew-x === -1 && Math.abs(yNew-y)===1){
+                    setX(xNew);
+                    setStepsBlack([...stepsBlack, id[1] + String.fromCharCode('h'.charCodeAt(0) - yNew) + (xNew+1)]);
+                    setY(yNew);
+                    refreshBoard(xPrev, yPrev, xNew, yNew);
+                }
+                break;
+        }
+    };
+
+    useEffect(() => {
+
+    }, [board]);
+
+    return (
+        <>
+            {!isHidden ?
+                <>
+                    <div
+                        draggable
+                        onDragEnd={handleDragEnd}
+                        style={{
+                            position: "absolute",
+                            top: x * 60,
+                            left: y * 60
+                        }}>
+                        <img src={figure} style={{ width: 60, height: 60 }} />
+                    </div>
+                </> :
+                <></>
+            }
+        </>
+    );
+}
+
+export default Figure;
