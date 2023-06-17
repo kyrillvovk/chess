c88ea96421c8f261fec245ee90f9f1c15c1cdf0e
Kyrill Vovk <kyrill07.vovk@gmail.com>
2023-06-17 14:34:37 +0200

    push

---
+++ src/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Menu(){
+    const [gameStarted, setGameStarted] = useState(false);
+    return (
+        <><button></button></>
+    );
+}
+
+export default Menu;
